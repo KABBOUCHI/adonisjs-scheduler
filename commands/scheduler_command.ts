c03e613fb7147eef8d8140875cc54816f8499289
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2024-11-30 21:55:29 +0200

    closes #11

--- commands/scheduler_command.ts
+++ commands/scheduler_command.ts
@@ -1,9 +1,9 @@
 import { BaseCommand, FsLoader, Kernel } from '@adonisjs/core/ace'
 import cron from 'node-cron'
-import AsyncLock from 'async-lock';
-import { CommandOptions } from '@adonisjs/core/types/ace';
+import AsyncLock from 'async-lock'
+import { CommandOptions } from '@adonisjs/core/types/ace'
 
-const lock = new AsyncLock();
+const lock = new AsyncLock()
 
 interface IRunOptions {
   enabled: boolean
@@ -13,14 +13,13 @@ interface IRunOptions {
 }
 
 const run = async (cb: () => any | PromiseLike<any>, options: IRunOptions) => {
-  if (!options.enabled)
-    return await cb()
+  if (!options.enabled) return await cb()
 
   if (lock.isBusy(options.key)) {
     if (options.onBusy) {
       await options.onBusy()
     }
-    return;
+    return
   }
 
   await lock.acquire(options.key, cb, { maxPending: 1, timeout: options.timeout })
@@ -29,15 +28,11 @@ const run = async (cb: () => any | PromiseLike<any>, options: IRunOptions) => {
 export default class SchedulerCommand extends BaseCommand {
   static commandName = 'scheduler:run'
   static description = ''
-  static aliases: string[] = [
-    'scheduler:work',
-    'schedule:work',
-    'schedule:run',
-  ]
+  static aliases: string[] = ['scheduler:work', 'schedule:work', 'schedule:run']
 
   static options: CommandOptions = {
     startApp: true,
-    staysAlive: true
+    staysAlive: true,
   }
 
   tasks: cron.ScheduledTask[] = []
@@ -51,74 +46,83 @@ export default class SchedulerCommand extends BaseCommand {
   }
 
   public async run() {
-    const schedule = await this.app.container.make("scheduler")
-    const logger = await this.app.container.make("logger")
+    const schedule = await this.app.container.make('scheduler')
+    const logger = await this.app.container.make('logger')
 
     if (schedule.onStartingCallback) {
       await schedule.onStartingCallback()
     }
 
     for (let index = 0; index < schedule.items.length; index++) {
-      const command = schedule.items[index];
+      const command = schedule.items[index]
       this.tasks.push(
-        cron.schedule(command.expression, async () => {
-          try {
-            switch (command.type) {
-              case "command":
-                const ace = new Kernel(this.app)
+        cron.schedule(
+          command.expression,
+          async () => {
+            try {
+              switch (command.type) {
+                case 'command':
+                  const ace = new Kernel(this.app)
 
-                this.app.rcFile.commands.forEach((commandModule) => {
-                  ace.addLoader(() =>
-                    typeof commandModule === 'function' ? commandModule() : this.app.import(commandModule)
-                  )
-                })
+                  this.app.rcFile.commands.forEach((commandModule) => {
+                    ace.addLoader(() =>
+                      typeof commandModule === 'function'
+                        ? commandModule()
+                        : this.app.import(commandModule)
+                    )
+                  })
 
-                const fsLoader = new FsLoader<typeof BaseCommand>(this.app.commandsPath())
-                ace.addLoader({
-                  async getMetaData() {
-                    if (!command.commandName || !ace.getCommand(command.commandName)) {
-                      return fsLoader.getMetaData()
-                    }
-                    return []
-                  },
-                  getCommand(command) {
-                    return fsLoader.getCommand(command)
-                  },
-                })
-                await run(() => ace.exec(command.commandName, command.commandArgs), {
-                  enabled: command.config.withoutOverlapping,
-                  timeout: command.config.expiresAt,
-                  key: `${index}-${command.commandName}-${command.commandArgs}`,
-                  onBusy: () => {
-                    logger.warn(`Command ${index}-${command.commandName}-${command.commandArgs} is busy`)
-                  }
-                })
-                break;
+                  const fsLoader = new FsLoader<typeof BaseCommand>(this.app.commandsPath())
+                  ace.addLoader({
+                    async getMetaData() {
+                      if (!command.commandName || !ace.getCommand(command.commandName)) {
+                        return fsLoader.getMetaData()
+                      }
+                      return []
+                    },
+                    getCommand(command) {
+                      return fsLoader.getCommand(command)
+                    },
+                  })
+                  await run(() => ace.exec(command.commandName, command.commandArgs), {
+                    enabled: command.config.withoutOverlapping,
+                    timeout: command.config.expiresAt,
+                    key: `${index}-${command.commandName}-${command.commandArgs}`,
+                    onBusy: () => {
+                      logger.warn(
+                        `Command ${index}-${command.commandName}-${command.commandArgs} is busy`
+                      )
+                    },
+                  })
+                  break
 
-              case "callback":
-                await run(() => command.callback(), {
-                  enabled: command.config.withoutOverlapping,
-                  timeout: command.config.expiresAt,
-                  key: `${index}-callback`,
-                  onBusy: () => {
-                    logger.warn(`Callback ${index} is busy`)
-                  }
-                })
+                case 'callback':
+                  await run(() => command.callback(), {
+                    enabled: command.config.withoutOverlapping,
+                    timeout: command.config.expiresAt,
+                    key: `${index}-callback`,
+                    onBusy: () => {
+                      logger.warn(`Callback ${index} is busy`)
+                    },
+                  })
 
-              default:
-                break;
+                default:
+                  break
+              }
+            } catch (error) {
+              logger.error(error)
             }
-          } catch (error) {
-            logger.error(error)
+          },
+          {
+            scheduled: command.config.enabled,
+            timezone: command.config.timezone,
+            runOnInit: command.config.enabled && command.config.immediate,
           }
-        }, {
-          scheduled: command.config.enabled,
-          runOnInit: command.config.enabled && command.config.immediate
-        })
+        )
       )
     }
 
-    logger.info(`Schedule worker started successfully.`);
+    logger.info(`Schedule worker started successfully.`)
 
     if (schedule.onStartedCallback) {
       await schedule.onStartedCallback()
--- src/scheduler.ts
+++ src/scheduler.ts
@@ -1,240 +1,305 @@
-import { BaseCommand } from "@adonisjs/core/ace";
-import { everyHour, everyMinute, every } from "node-cron-expression"
+import { BaseCommand } from '@adonisjs/core/ace'
+import { everyHour, everyMinute, every } from 'node-cron-expression'
 
 abstract class BaseSchedule {
-    abstract type: string;
-    expression: string = "* * * * *";
-    config = {
-        enabled: true,
-        immediate: false,
-        withoutOverlapping: false,
-        expiresAt: 3600000
-    }
+  abstract type: string
+  expression: string = '* * * * *'
+  config = {
+    enabled: true,
+    immediate: false,
+    withoutOverlapping: false,
+    expiresAt: 3600000,
+    timezone: undefined as string | undefined,
+  }
 
-    public skip(state: boolean = true) {
-        this.config.enabled = !state
+  public timezone(timezone: string) {
+    this.config.timezone = timezone
 
-        return this;
-    }
+    return this
+  }
 
-    public immediate(state: boolean = true) {
-        this.config.immediate = state
+  public skip(state: boolean = true) {
+    this.config.enabled = !state
 
-        return this;
-    }
+    return this
+  }
 
-    public withoutOverlapping(expiresAt: number = 3600000) {
-        this.config.withoutOverlapping = true
-        this.config.expiresAt = expiresAt;
+  public immediate(state: boolean = true) {
+    this.config.immediate = state
 
-        return this;
-    }
+    return this
+  }
 
-    public everyMinutes(minutes: number) {
-        this.expression = every(minutes).minutes().toString()
+  public withoutOverlapping(expiresAt: number = 3600000) {
+    this.config.withoutOverlapping = true
+    this.config.expiresAt = expiresAt
 
-        return this;
-    }
+    return this
+  }
 
-    public everyMinute() {
-        this.expression = everyMinute().toString()
+  public everyMinutes(minutes: number) {
+    this.expression = every(minutes).minutes().toString()
 
-        return this;
-    }
+    return this
+  }
 
-    public everyTwoMinutes() {
-        return this.everyMinutes(2)
-    }
+  public everyMinute() {
+    this.expression = everyMinute().toString()
 
-    public everyThreeMinutes() {
-        return this.everyMinutes(3)
-    }
+    return this
+  }
 
-    public everyFourMinutes() {
-        return this.everyMinutes(4)
-    }
+  public everyTwoMinutes() {
+    return this.everyMinutes(2)
+  }
 
-    public everyFiveMinutes() {
-        return this.everyMinutes(5)
-    }
+  public everyThreeMinutes() {
+    return this.everyMinutes(3)
+  }
 
-    public everyTenMinutes() {
-        return this.everyMinutes(10)
-    }
+  public everyFourMinutes() {
+    return this.everyMinutes(4)
+  }
 
-    public everyFifteenMinutes() {
-        return this.everyMinutes(15)
-    }
+  public everyFiveMinutes() {
+    return this.everyMinutes(5)
+  }
 
-    public everyThirtyMinutes() {
-        return this.everyMinutes(30)
-    }
+  public everyTenMinutes() {
+    return this.everyMinutes(10)
+  }
 
-    public hourly() {
-        this.expression = everyHour().toString()
+  public everyFifteenMinutes() {
+    return this.everyMinutes(15)
+  }
 
-        return this;
-    }
+  public everyThirtyMinutes() {
+    return this.everyMinutes(30)
+  }
 
-    public everyHours(hours: number) {
-        this.expression = every(hours).hours().toString()
+  public hourly() {
+    this.expression = everyHour().toString()
 
-        return this;
-    }
+    return this
+  }
 
-    public everyTwoHours() {
-        return this.everyHours(2);
-    }
+  public everyHours(hours: number) {
+    this.expression = every(hours).hours().toString()
 
-    public everyThreeHours() {
-        return this.everyHours(3);
-    }
+    return this
+  }
 
-    public everyFourHours() {
-        return this.everyHours(5);
-    }
+  public everyTwoHours() {
+    return this.everyHours(2)
+  }
 
-    public everyFiveHours() {
-        return this.everyHours(6);
-    }
+  public everyThreeHours() {
+    return this.everyHours(3)
+  }
 
-    public everySixHours() {
-        return this.everyHours(6);
-    }
+  public everyFourHours() {
+    return this.everyHours(5)
+  }
 
-    public daily() {
-        this.expression = '0 0 * * *'
+  public everyFiveHours() {
+    return this.everyHours(6)
+  }
 
-        return this;
-    }
+  public everySixHours() {
+    return this.everyHours(6)
+  }
 
-    public weekly() {
-        this.expression = '0 0 * * 0'
+  public daily() {
+    this.expression = '0 0 * * *'
 
-        return this;
-    }
+    return this
+  }
 
-    public monthly() {
-        this.expression = '0 0 1 * *'
+  public weekly() {
+    this.expression = '0 0 * * 0'
 
-        return this;
-    }
+    return this
+  }
 
-    public quarterly() {
-        this.expression = '0 0 1 */3 *'
+  public monthly() {
+    this.expression = '0 0 1 * *'
 
-        return this;
-    }
+    return this
+  }
 
+  public quarterly() {
+    this.expression = '0 0 1 */3 *'
 
-    public yearly() {
-        this.expression = '0 0 1 1 *'
+    return this
+  }
 
-        return this;
-    }
+  public yearly() {
+    this.expression = '0 0 1 1 *'
 
-    public everySecond() {
-        this.expression = "* * * * * *"
+    return this
+  }
 
-        return this;
-    }
+  public everySecond() {
+    this.expression = '* * * * * *'
 
-    public everySeconds(second: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49 | 50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59) {
-        this.expression = `*/${second} * * * * *`
+    return this
+  }
 
-        return this;
-    }
+  public everySeconds(
+    second:
+      | 1
+      | 2
+      | 3
+      | 4
+      | 5
+      | 6
+      | 7
+      | 8
+      | 9
+      | 10
+      | 11
+      | 12
+      | 13
+      | 14
+      | 15
+      | 16
+      | 17
+      | 18
+      | 19
+      | 20
+      | 21
+      | 22
+      | 23
+      | 24
+      | 25
+      | 26
+      | 27
+      | 28
+      | 29
+      | 30
+      | 31
+      | 32
+      | 33
+      | 34
+      | 35
+      | 36
+      | 37
+      | 38
+      | 39
+      | 40
+      | 41
+      | 42
+      | 43
+      | 44
+      | 45
+      | 46
+      | 47
+      | 48
+      | 49
+      | 50
+      | 51
+      | 52
+      | 53
+      | 54
+      | 55
+      | 56
+      | 57
+      | 58
+      | 59
+  ) {
+    this.expression = `*/${second} * * * * *`
 
-    public everyFiveSeconds() {
-        return this.everySeconds(5);
-    }
+    return this
+  }
 
-    public everyTenSeconds() {
-        return this.everySeconds(10);
-    }
+  public everyFiveSeconds() {
+    return this.everySeconds(5)
+  }
 
-    public everyFifteenSeconds() {
-        return this.everySeconds(15);
-    }
+  public everyTenSeconds() {
+    return this.everySeconds(10)
+  }
 
-    public everyThirtySeconds() {
-        return this.everySeconds(30);
-    }
+  public everyFifteenSeconds() {
+    return this.everySeconds(15)
+  }
 
-    public cron(expression: string) {
-        this.expression = expression;
+  public everyThirtySeconds() {
+    return this.everySeconds(30)
+  }
 
-        return this;
-    }
+  public cron(expression: string) {
+    this.expression = expression
+
+    return this
+  }
 }
 
 class ScheduleCommand extends BaseSchedule {
-    type: "command" = "command";
+  type: 'command' = 'command'
 
-    commandName: string;
-    commandArgs: string[];
+  commandName: string
+  commandArgs: string[]
 
-    constructor(commandName: string, commandArgs: string[] = []) {
-        super();
+  constructor(commandName: string, commandArgs: string[] = []) {
+    super()
 
-        this.commandName = commandName;
-        this.commandArgs = commandArgs;
-    }
+    this.commandName = commandName
+    this.commandArgs = commandArgs
+  }
 }
 
 class ScheduleCallback extends BaseSchedule {
-    type: "callback" = "callback";
+  type: 'callback' = 'callback'
 
-    callback: Function;
+  callback: Function
 
-    constructor(callback: Function) {
-        super();
+  constructor(callback: Function) {
+    super()
 
-        this.callback = callback;
-    }
+    this.callback = callback
+  }
 }
 
 export class Scheduler {
-    items: (ScheduleCallback | ScheduleCommand)[] = []
+  items: (ScheduleCallback | ScheduleCommand)[] = []
 
-    onStartingCallback?: () => void|Promise<void>;
-    onStartedCallback?: () => void|Promise<void>;
+  onStartingCallback?: () => void | Promise<void>
+  onStartedCallback?: () => void | Promise<void>
 
-    public command(name: string | typeof BaseCommand, args: string[] = []) {
+  public command(name: string | typeof BaseCommand, args: string[] = []) {
+    let newCommand = new ScheduleCommand(typeof name === 'string' ? name : name.commandName, args)
 
-        let newCommand = new ScheduleCommand(typeof name === "string" ? name : name.commandName, args);
+    this.items.push(newCommand)
 
-        this.items.push(newCommand)
+    return newCommand
+  }
 
-        return newCommand
+  public call(callback: Function) {
+    let newCommand = new ScheduleCallback(callback)
+
+    this.items.push(newCommand)
+
+    return newCommand
+  }
+
+  public withoutOverlapping(callback: () => void, config = { expiresAt: 3600000 }) {
+    const lastLength = this.items.length
+    callback()
+    const currentLength = this.items.length
+
+    const newItems = this.items.slice(lastLength, currentLength)
+
+    for (const item of newItems) {
+      item.withoutOverlapping(config.expiresAt)
     }
+  }
 
-    public call(callback: Function) {
+  public onStarting(callback: () => void | Promise<void>) {
+    this.onStartingCallback = callback
+  }
 
-        let newCommand = new ScheduleCallback(callback);
-
-        this.items.push(newCommand)
-
-        return newCommand
-    }
-
-    public withoutOverlapping(callback: () => void, config = { expiresAt: 3600000 }) {
-        const lastLength = this.items.length
-        callback()
-        const currentLength = this.items.length
-
-        const newItems = this.items.slice(lastLength, currentLength);
-
-        for (const item of newItems) {
-            item.withoutOverlapping(config.expiresAt)
-        }
-    }
-
-    public onStarting(callback: () => void|Promise<void>) {
-        this.onStartingCallback = callback
-    }
-
-    public onStarted(callback: () => void|Promise<void>) {
-        this.onStartedCallback = callback
-    }
+  public onStarted(callback: () => void | Promise<void>) {
+    this.onStartedCallback = callback
+  }
 }
